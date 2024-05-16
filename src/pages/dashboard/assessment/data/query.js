6b8cbca86b188d4e87e61d4fe193e9b2f73e653d
Lilyy22 <gelilahhamid2@gmail.com>
2024-05-16 02:46:03 -0700

    fix: assessment 50% complete

--- src/pages/dashboard/assessment/data/query.js
+++ src/pages/dashboard/assessment/data/query.js
@@ -24,6 +24,26 @@ export const GETASSESSMENT = gql`
   }
 `;
 
+export const GETASSESSMENTNOPAGINATION = gql`
+  query GETASSESSMENT($courseId: String!) {
+    question(where: { course_id: { exact: $courseId } }) {
+      id
+      question_text
+      course {
+        name
+      }
+      answers {
+        id
+        answer_text
+        is_true
+      }
+      aggregate {
+        count
+      }
+    }
+  }
+`;
+
 export const GETQUESTION = gql`
   query GETQUESTION($questionId: String!) {
     question(where: { id: { exact: $questionId } }) {
